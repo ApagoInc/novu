74e06c1d65590cdb6e2a9df7525a9bd31d8e4731
Jake Watkins (novu.apagocloud.net EC2) <jakewatkins8@gmail.com>
2024-12-19 01:20:48 +0000

    add new stakeholder completion events, inadvertently removed in updates

--- apps/api/src/app/apago/data/stakeholderStages.ts
+++ apps/api/src/app/apago/data/stakeholderStages.ts
@@ -1,32 +1,41 @@
-import {
-  DigestUnitEnum,
-  DigestTypeEnum,
-} from '@novu/shared';
+import { DigestUnitEnum, DigestTypeEnum } from '@novu/shared';
 
-const normalDigest = (digestTime: number, units: DigestUnitEnum) => (
-  {
-    value: digestTime,
-    valueUnit: units,
-  }
-)
+const normalDigest = (digestTime: number, units: DigestUnitEnum) => ({
+  value: digestTime,
+  valueUnit: units,
+});
 
 const stakeholderStages = [
   {
-    label: "Resolve Preflight",
-    value: "Preflight1_ApplyFix",
-    digest: normalDigest(5, DigestUnitEnum.SECONDS)
+    label: 'Resolve Preflight',
+    value: 'Preflight1_ApplyFix',
+    digest: normalDigest(5, DigestUnitEnum.SECONDS),
   },
   {
-    label: "Approve Content",
-    value: "Preflight1_Signoff",
-    digest: normalDigest(5, DigestUnitEnum.SECONDS)
-
+    label: 'Approve Content',
+    value: 'Preflight1_Signoff',
+    digest: normalDigest(5, DigestUnitEnum.SECONDS),
   },
   {
-    label: "Approve to Print",
-    value: "Preflight2_Signoff",
-    digest: normalDigest(5, DigestUnitEnum.SECONDS)
-  }
-]
+    label: 'Approve to Print',
+    value: 'Preflight2_Signoff',
+    digest: normalDigest(5, DigestUnitEnum.SECONDS),
+  },
+  {
+    label: 'Resolve Preflight Complete',
+    value: 'resolve-preflight-complete',
+    digest: normalDigest(5, DigestUnitEnum.SECONDS),
+  },
+  {
+    label: 'Approve Content Complete',
+    value: 'approve-content-complete',
+    digest: normalDigest(5, DigestUnitEnum.SECONDS),
+  },
+  {
+    label: 'Approve to Print Complete',
+    value: 'approve-to-print-complete',
+    digest: normalDigest(5, DigestUnitEnum.SECONDS),
+  },
+];
 
 export default stakeholderStages;
